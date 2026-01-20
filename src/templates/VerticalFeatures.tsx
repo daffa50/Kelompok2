e4ad84dee6d783735226246902735c35a1e2f969
Alif FH <aliffahmihidayatullah@gmail.com>
2026-01-20 20:35:27 +0700

    update2

--- src/templates/VerticalFeatures.tsx
+++ src/templates/VerticalFeatures.tsx
@@ -3,27 +3,29 @@ import { Section } from '../layout/Section';
 
 const VerticalFeatures = () => (
   <Section
-    title="Your title here"
-    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus malesuada nisi tellus, non imperdiet nisi tempor at."
+    title="Kasur Nyaman untuk Tidur Berkualitas"
+    description="Kasur dirancang untuk memberikan kenyamanan maksimal, menopang tubuh dengan sempurna, dan membantu meningkatkan kualitas tidur setiap malam."
   >
     <VerticalFeatureRow
-      title="Your title here"
-      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
-      image="/assets/images/feature.svg"
-      imageAlt="First feature alt text"
+      title="Empuk & Ergonomis"
+      description="Kasur dibuat dengan material berkualitas yang menyesuaikan bentuk tubuh, mengurangi tekanan pada punggung, dan membuat tidur lebih nyenyak."
+      image="/assets/images/kasur3.jpg"
+      imageAlt="Kasur empuk dan ergonomis"
     />
+
     <VerticalFeatureRow
-      title="Your title here"
-      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
-      image="/assets/images/feature2.svg"
-      imageAlt="Second feature alt text"
+      title="Sirkulasi Udara Optimal"
+      description="Desain kasur mendukung sirkulasi udara yang baik sehingga tidak panas, tetap sejuk, dan nyaman digunakan sepanjang malam."
+      image="/assets/images/kasur2.jpg"
+      imageAlt="Sirkulasi udara kasur"
       reverse
     />
+
     <VerticalFeatureRow
-      title="Your title here"
-      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
-      image="/assets/images/feature3.svg"
-      imageAlt="Third feature alt text"
+      title="Tahan Lama & Mudah Dirawat"
+      description="Menggunakan bahan yang kuat dan awet, kasur tidak mudah kempes serta mudah dibersihkan untuk penggunaan jangka panjang."
+      image="/assets/images/kasur1.jpg"
+      imageAlt="Kasur tahan lama"
     />
   </Section>
 );
